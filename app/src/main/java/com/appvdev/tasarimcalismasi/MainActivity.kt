1d91d9d9199f177e763fe5667231a69980861d5b
Omer Dogan <omerdogan.27gs@gmail.com>
2023-04-06 14:08:11 +0300

    Version 1.0.0

---
+++ app/src/main/java/com/appvdev/tasarimcalismasi/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.appvdev.tasarimcalismasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //R = res klasörü (Kodlama içinde gerekli)
+        //@ = res klasörü (Xml dosyası içerisinde geçerli)
+
+    }
+}
